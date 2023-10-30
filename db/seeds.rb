e065746af1f5b6a9f363caeda3d2058c2462dd19
Derek Chavez <frrenose17@yahoo.com>
2023-10-30 16:20:16 -0600

    feat: add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+customer = Customer.create(first_name: "Bob", last_name: "Gu", email: "bg@gmail.com", address: "1234 Rails St")
+
+tea = Tea.create(:title => "Earl Grey", :description => "Tea", :temp => 200, :brew_time => 5)
+tea2 = Tea.create(:title => "Green Tea", :description => "Tea", :temp => 201, :brew_time => 4)
+subscription = Subscription.create(:title => "Earl Grey", :price => 10.00, :status => 0, :frequency => 1, :customer_id => customer.id, :tea_id => tea.id)
+subscription2 = Subscription.create(:title => "Green Tea", :price => 10.00, :status => 1, :frequency => 1, :customer_id => customer.id, :tea_id => tea2.id)
+
